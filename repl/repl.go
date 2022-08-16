42750de894df4543d2087cd649303407491766a3
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2022-08-16 17:09:06 +0900

    20220816: REPL作成(ReadしてPrintのみ)

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"kb_monkey/lexer"
+	"kb_monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Println(PROMPT)
+
+		// 入力待機
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
