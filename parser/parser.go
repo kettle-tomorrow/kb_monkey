921325670392dc63d3f5fef75692c58686c6d914
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2022-09-03 06:34:40 +0900

    20220903: parser.parseExpressionStatementを追加

--- parser/parser.go
+++ parser/parser.go
@@ -130,3 +130,14 @@ func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
 }
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
