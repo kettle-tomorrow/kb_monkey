4b2769cbd8e19a302cbb70ca25b95b0b96b44c3f
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2022-09-02 12:34:06 +0900

    20220902: parseStatementの分岐にtoken.IDENTを追加

--- parser/parser.go
+++ parser/parser.go
@@ -53,6 +53,8 @@ func (p *Parser) parseStatement() ast.Statement {
 		return p.parseLetStatement()
 	case token.RETURN:
 		return p.parseReturnStatement()
+	case token.IDENT:
+		return p.parseExpressionStatement()
 	default:
 		return nil
 	}
